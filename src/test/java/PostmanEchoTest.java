d13e731b7e4ef31aaa66077c520696efde8fa4d8
KozlovskyS <skozmail@gmail.com>
2025-02-04 20:05:41 +0300

    added test

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void testRequestDemoLatin (){
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("demo data")    // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("demo data"))
+        ;
+    }
+
+    @Test
+    public void testRequestDemoCyrillic (){
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("тестовые данные")    // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("тестовые данные"))
+        ;
+    }
+}
